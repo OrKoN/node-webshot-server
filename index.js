7f43d0d0a6b63feaec3ffed8172e7a353f6dfa41
OrKoN <alexei.rudenko@gmail.com>
2016-05-25 14:50:08 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const http = require('http');
+const PORT = 8080;
+const handler = require('./lib/handler');
+const server = http.createServer(handler);
+server.listen(PORT, () => console.log("Server listening on: http://localhost:%s", PORT));
